ca2d4582111fb48ee1b0ae3c19b4c163e84ed627
Vladimir Abramyants <vabramanc@gmaill.com>
2019-04-10 13:50:46 +0300

    Added new action: SIGIN

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,12 +1,68 @@
 <template>
     <div>
-        <h1>SignIn</h1>
+        <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md6>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>Вход</v-toolbar-title>
+                <v-spacer></v-spacer>
+              </v-toolbar>
+              <v-card-text>
+                <v-alert
+                  :value="error"
+                  type="warning"
+                >
+                  {{ error }}
+                </v-alert>
+                <v-form>
+                  <v-text-field prepend-icon="person" name="login" label="Login" type="email" required v-model="email"></v-text-field>
+                  <v-text-field id="password" prepend-icon="lock" name="password" label="Password" type="password" required v-model="password"></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" @click.prevent="signIn" :disabled="proccessing">Войти</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
     </div>
 </template>
 
 <script>
 export default {
     
+    data() {
+        return {
+            email: null,
+            password: null
+        }
+    },
+    computed:{
+      error() {
+        return this.$store.getters.getError
+      } ,
+      proccessing() {
+        return this.$store.getters.getProccessing
+      },
+      isUserAutheticated() {
+        return this.$store.getters.isUserAuth
+      }                                                           
+    },
+    watch:{
+      isUserAutheticated(val) {
+        if (val === true) {
+          this.$router.push("/")
+        }
+      }
+    },
+    methods: {
+      signIn() {
+        this.$store.dispatch('SIGNIN', {email: this.email, password: this.password})
+      }
+    }
 }
 </script>
 
