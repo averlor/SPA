e969ee17cd58da9ab563df3cfa997c61ef6a3520
Vladimir Abramyants <vabramanc@gmaill.com>
2019-04-10 16:02:24 +0300

    Added new handle of firebase and added vuetify-confirm

--- src/main.js
+++ src/main.js
@@ -3,12 +3,24 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Vuetify from 'vuetify'
+import VuetifyConfirm from 'vuetify-confirm'
 import firebaseConfig from './config/firebase'
 import firebase from 'firebase'
 
 import 'vuetify/dist/vuetify.min.css' 
+import { createDecipher } from 'crypto';
 
 Vue.use(Vuetify)
+
+Vue.use(VuetifyConfirm, {
+  buttonTrueText: 'Да',
+  buttonFalseText: 'Нет',
+  color: 'warning',
+  icon: 'warning',
+  title: 'Warning',
+  width: 350,
+  property: '$confirm'
+})
 
 Vue.config.productionTip = false
 firebase.initializeApp(firebaseConfig)
@@ -16,5 +28,11 @@ firebase.initializeApp(firebaseConfig)
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created() {
+    let vm = this
+    firebase.auth().onAuthStateChanged(function(user) {
+      vm.$store.dispatch('STATE_CHANGE', user)
+    });
+  }
 }).$mount('#app')
