14a2127a0bc7416fd5dbf808c80715ec7c2757c3
Vladimir Abramyants <vabramanc@gmaill.com>
2019-04-10 13:54:19 +0300

    Added new store: menuItems. While not use

--- src/store/general.js
+++ src/store/general.js
@@ -1,7 +1,46 @@
 export default {
     state: {
         proccessing: false,
-        error: null
+        error: null,
+        menuItems: [
+            
+            {
+                id: 1,
+                icon: 'visibility',
+                title: 'Читать',
+                route: '/books'
+            },
+            {
+                id: 2,
+                icon: 'extension',
+                title: 'Учить слова',
+                route: '/words'
+            },
+            {
+                id: 3,
+                icon: 'account_circle',
+                title: 'Мой кабинет',
+                route: '/profile'
+            },
+            {
+                id: 4,
+                icon: 'exit_to_app',
+                title: 'Выйти',
+                route: '/logout'
+            },
+            {
+                id: 5,
+                icon: 'input',
+                title: 'Войти',
+                route: '/signin'
+            },
+            {
+                id: 6,
+                icon: 'lock_open',
+                title: 'Зарегистрироваться',
+                route: '/signup'
+            }
+         ]
     },
     mutations: {
         SET_PROCCESSING(state, payload) {
@@ -15,6 +54,13 @@ export default {
         }
     },
     getters: {
+        getMenu: state => state.menuItems,
+        getMenuUserSignIn: state => {
+            state.menuItems.map(item => item.id < 4)
+        },
+        getMenuUserSignOut: state => {
+            state.menuItems.map(item => item.id > 5)
+        },
         getProccessing: state => {
             return state.proccessing
         },
