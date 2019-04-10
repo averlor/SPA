65bd247b18f6eaf12d6390b319e94a95153a48c4
Vladimir Abramyants <vabramanc@gmaill.com>
2019-04-10 16:03:35 +0300

    Added new action for sign out

--- src/store/user.js
+++ src/store/user.js
@@ -14,14 +14,19 @@ export default {
         SET_USER(state, payload) {
             state.user.isAuth = true
             state.user.uid = payload
+        },
+        UNSET_USER(state) {
+            state.user = {
+                isAuth: false,
+                uid: null
+            }
         }
     },
     actions: {
-        SIGNUP({ commit }, payload) {
+        SIGN_UP({ commit }, payload) {
             commit('SET_PROCCESSING', true)
             firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password)
-            .then(user => {
-                commit('SET_USER', user.uid)
+            .then( () => {
                 commit('SET_PROCCESSING', false)
                 
             })
@@ -30,17 +35,27 @@ export default {
                 commit('SET_ERROR', error.message)
               });
         },
-        SIGNIN({commit}, payload) {
+        SIGN_IN({commit}, payload) {
             firebase.auth().signInWithEmailAndPassword(payload.email, payload.password)
-            .then(user => {
-                commit('SET_USER', user.uid)
+            .then( () => {
                 commit('SET_PROCCESSING', false)
+                commit('CLEAR_ERROR')
                 
             })
             .catch(function(error) {
                 commit('SET_PROCCESSING', false)
                 commit('SET_ERROR', error.message)
               });
+        },
+        SIGN_OUT() {
+            firebase.auth().signOut()
+        },
+        STATE_CHANGE({commit}, payload) {
+            if (payload) {
+                commit('SET_USER', payload.uid)
+            } else {
+                commit('UNSET_USER')
+            }
         }
     }
 }
