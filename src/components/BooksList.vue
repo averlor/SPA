0d1985079dbd697e3190345466d23a4cc0efc95f
Vladimir Abramyants <vabramanc@gmaill.com>
2019-04-10 18:27:54 +0300

    Created new component

---
+++ src/components/BooksList.vue
@@ -0,0 +1,70 @@
+<template>
+    <v-container grid-list-md>
+        <v-layout row wrap>
+            <v-flex xs12 sm10 md8 offset-sm1 offset-md2>
+                <v-container fluid>
+                    <v-layout row>
+                        <v-flex>
+                            <v-text-field label="Search" v-model="searchTerm"></v-text-field>
+                        </v-flex>
+                    </v-layout>
+                </v-container>
+            </v-flex>
+            <v-flex v-for="book in filrteredBooks" :key="book.id" xs12 sm10 md8 offset-sm1 offset-md2>
+                <v-card color="info" class="white--text">
+                    <v-container fluid>
+                        <v-layout row>
+                            <v-flex xs4 md3 >
+                                <v-card-media src= 'https://media.bloomsbury.com/rep/bj/9781408855652.jpeg'>
+                                </v-card-media>
+                            </v-flex>
+                            <v-flex xs8 md9>
+                                <v-card-title>
+                                    <div>
+                                        <div class="headline">
+                                            {{ book.title }}
+                                        </div>
+                                        <div>{{ book.desciption }}</div>
+                                        <v-divider class="white"></v-divider>
+                                    </div>
+                                </v-card-title>
+                                <v-card-actions>
+                                    <v-rating v-model="book.rating" color="yellow" readonly dense half-increments></v-rating>
+                                    <div class="ml-1">
+                                        <span>{{ book.rating }}</span>
+                                        <span>({{ book.ratingsCount }})</span>
+                                    </div>
+                                    <v-spacer></v-spacer>
+                                    <v-btn flat class="primary">ПОДРОБНЕЕ...</v-btn>  
+                                </v-card-actions>
+                            </v-flex>
+                        </v-layout>    
+                    </v-container>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            searchTerm: null
+        }
+    },
+    computed: {
+        books() {
+            return this.$store.getters.getBooks
+        },
+        filrteredBooks() {
+            let books = this.books
+            if (this.searchTerm) {
+                books = books.filter(book => book.title.toLowerCase().indexOf(this.searchTerm.toLowerCase()) >= 0) 
+            }
+            return books;
+        }
+    }
+}
+</script>
+
