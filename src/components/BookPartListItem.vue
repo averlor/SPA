7d184339500b902a462faf9954563fa04c8054da
Vladimir Abramyants <vabramanc@gmaill.com>
2019-04-11 17:48:28 +0300

    Created new component: BookPartList

---
+++ src/components/BookPartListItem.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-card color="accent" class="white--text">
+        <v-card-title primary-title class="headline">
+            {{ part.title }}
+        </v-card-title>
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn flat class="white" :to="part.link">Открыть</v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+export default {
+    props: {
+        part: {
+            type: Object,
+            required: true
+        },
+        bookId: {
+            type: String,
+            required: true
+
+        }
+    }
+}
+</script>
