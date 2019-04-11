ab601937f1b2b873763527151b56e0cd44f4afe7
Vladimir Abramyants <vabramanc@gmaill.com>
2019-04-11 17:52:49 +0300

    Edit homaPage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,12 +1,43 @@
 <template>
-    
+    <div>
+        <v-container grid-list-md>
+            <v-layout row wrap>
+                <!-- hello-message start -->
+                <v-flex xs12 sm10 md8 offset-sm1 offset-md2>
+                    <v-container fluid>
+                        <v-layout row>
+                            <v-flex>
+                                <div>
+                                    <h2>Hey, Hello!</h2>
+                                    <h3>
+                                        Are you to us? Come on soon, but it was already waiting!
+                                    </h3>
+                                </div>
+                            </v-flex>
+                        </v-layout>
+                    </v-container>
+                </v-flex>
+                <!-- Hello-message end -->
+
+                <!-- Content start -->
+                <v-flex xs12 sm10 md8 offset-sm1 offset-md2>
+                    <HelloCard/>
+                </v-flex>
+                <!-- Content end -->
+            </v-layout>
+    </v-container>
+    </div>
 </template>
 
 
 <script>
 
+import HelloCard from '@/components/MainPage.vue'
 
 export default {
-    name: 'home'
+    name: 'home',
+    components: {
+        HelloCard
+    }
 }
 </script>
